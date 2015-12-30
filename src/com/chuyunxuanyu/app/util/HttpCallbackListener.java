8f7e299e2f0d74ae7d2d560ecf9724ae0b3a85c9
liuyang <1396924332@qq.com>
2015-12-30 22:17:03 +0800

    新增http请求类

---
+++ src/com/chuyunxuanyu/app/util/HttpCallbackListener.java
@@ -0,0 +1,14 @@
+package com.chuyunxuanyu.app.util;
+
+public interface HttpCallbackListener {
+
+	/*
+	 * author:liuyang
+	 * time:2015-12-30
+	 * description:月儿妹子别忘记我
+	 */
+	void onFinish();
+	
+	void onError();
+	
+}
